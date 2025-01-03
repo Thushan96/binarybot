6ab6fe6ee93a948f6b54f42ce2ed3b8f7fbd5c9b
Thushan96 <thushanmadhulakshitha@gmail.com>
2025-01-03 12:45:43 +0530

    Update login

--- binarybot/src/app/login/page.tsx
+++ binarybot/src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import deriv from "../deriv.png";
+import aiTrader from "../aiTrader.png";
 
 export default function Login() {
   return (
@@ -22,18 +23,22 @@ export default function Login() {
           <p className="text-sm  mt-6">
             Binary Bot and Deriv Bot auto trading platforms.
           </p>
-          <p className="text-sm"> Start your algorithmic trading journey today!</p>
+          <p className="text-sm">
+            {" "}
+            Start your algorithmic trading journey today!
+          </p>
           <button className="bg-red-600 mt-6 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-700 w-full md:w-auto">
             NO TOKEN? CREATE AN ACCOUNT
           </button>
         </div>
 
+        {/* Vertical Divider */}
+        <div className="hidden md:block w-[1px] bg-gray-300 h-full mx-4"></div>
+
         {/* Right Section */}
-        <div className="md:w-1/2 rounded-lg p-6">
+        <div className="md:w-1/2 flex flex-col items-center justify-center text-center p-4">
           <div className="text-center md:w-1/2 rounded-lg p-6">
-            <h2 className="text-gray-800 text-lg font-semibold mb-4">
-              Login to Continue
-            </h2>
+            <Image width="200" height="120" src={aiTrader} alt="/" />
           </div>
           <div className="flex flex-col items-center mb-6">
             <button className="flex items-center justify-center w-full py-2 px-4 bg-gray-800 text-white rounded-md hover:bg-gray-700">
@@ -46,7 +51,7 @@ export default function Login() {
               placeholder="Enter API Token"
               className="w-full border border-gray-300 rounded-md py-2 px-4 focus:ring-2 focus:ring-blue-500 focus:outline-none mb-4"
             />
-            <button className="bg-gray-800 text-white py-2 px-4 rounded-md shadow-md hover:bg-gray-700 w-full">
+            <button className="bg-gray-800 text-white py-2 px-4 rounded-md shadow-md hover:bg-gray-700 w-[20rem]">
               SIGN IN
             </button>
           </div>
