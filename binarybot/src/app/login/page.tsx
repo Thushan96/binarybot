948d43629f14bb36a0e4bdad68e580ad77b897ab
Thushan96 <thushanmadhulakshitha@gmail.com>
2025-01-03 12:13:02 +0530

    Update login

--- binarybot/src/app/login/page.tsx
+++ binarybot/src/app/login/page.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import deriv from "../deriv.png";
 
 export default function Login() {
   return (
@@ -6,24 +8,22 @@ export default function Login() {
       <Head>
         <title>Login</title>
       </Head>
-      <div className="flex flex-col md:flex-row items-center justify-between bg-white rounded-lg shadow-lg p-8 w-full max-w-5xl" style={{ width: '800px', height: '500px' }}>
+      <div
+        className="flex flex-col md:flex-row items-center justify-between bg-white rounded-lg shadow-lg p-8 w-full max-w-5xl"
+        style={{ width: "810px", height: "500px" }}
+      >
         {/* Left Section */}
-        <div className="md:w-1/2 text-center md:text-left p-4">
+        <div className="md:w-1/2 flex flex-col items-center justify-center text-center p-4">
           <h1 className="text-2xl font-bold text-gray-800 mb-4">
             Free Binary Bot & Deriv Bot
           </h1>
           <p className="text-gray-600 mb-6">Click to start free AutoTrading</p>
-          <img
-            src="/binarybot-logo.png"
-            alt="BinaryBot"
-            className="w-48 mx-auto md:mx-0 mb-6"
-          />
-          <p className="text-gray-600">
+          <Image width="150" height="80" src={deriv} alt="/" />
+          <p className="text-sm  mt-6">
             Binary Bot and Deriv Bot auto trading platforms.
-            <br />
-            Start your algorithmic trading journey today!
           </p>
-          <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-700 w-full md:w-auto">
+          <p className="text-sm"> Start your algorithmic trading journey today!</p>
+          <button className="bg-red-600 mt-6 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-700 w-full md:w-auto">
             NO TOKEN? CREATE AN ACCOUNT
           </button>
         </div>
@@ -37,7 +37,8 @@ export default function Login() {
           </div>
           <div className="flex flex-col items-center mb-6">
             <button className="flex items-center justify-center w-full py-2 px-4 bg-gray-800 text-white rounded-md hover:bg-gray-700">
-              LOGIN WITH <span className="ml-2 font-bold text-red-500">DERIV</span>
+              LOGIN WITH{" "}
+              <span className="ml-2 font-bold text-red-500">DERIV</span>
             </button>
             <p className="text-gray-500 my-4">OR</p>
             <input
